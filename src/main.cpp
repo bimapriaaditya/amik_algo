d9f1c910280064cac208b0ecbab0da34c4cfc9a7
bimapriaaditya <bpabima@gmail.com>
2022-11-21 21:42:06 +0700

    swwitch case

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,81 @@
-// Example program
 #include <iostream>
 using namespace std;
-
-int main()
+main()
 {
+    char KodePulau;
+    int KodeKota;
+    // char adalah variabel yang isinya karakter diikuti dengan nama variabel
+    // yang akan dibuat
 
-    int x, y, z, a, temp_besar;
-
-    cout << "Masukan Bilangan Bulat" << endl;
-
-    cout << "Masukan Nilai x :" << endl;
-    cin >> x;
-
-    cout << "Masukan Nilai y :" << endl;
-    cin >> y;
-
-    cout << "Masukan Nilai z :" << endl;
-    cin >> z;
-
-    cout << "Masukan Nilai a :" << endl;
-    cin >> a;
-
-    if (x > y)
+    cout << "Masukan Kode Pulau : ";
+    // sourch diatas berfungsi untuk menampilkan " Masukan Kode Pulau"
+    cin >> KodePulau;
+    // berfungsi untu inputan kode pulau sesuai dengan case masing-masing
+    switch (KodePulau)
     {
-        temp_besar = x;
+    case 'J':
+        cout << "Masukan Kode Pulau Jawa :";
+        cin >> KodeKota;
+        switch (KodeKota)
+        {
+        case 1:
+            cout << "\n Jakarta";
+            break;
+        case 2:
+            cout << "\n Surabaya";
+            break;
+        case 3:
+            cout << "\n Jakarta";
+            break;
+        case 4:
+            cout << "\n Jakarta";
+            break;
+        default:
+            cout << "Kode Kota diPulU AWA sALAH";
+        }
+        break;
+    case 'S':
+        cout << "Masukan Kode Pulau Sumatra ";
+        cin >> KodeKota;
+        switch (KodeKota)
+        {
+        case 1:
+            cout << "\n Medan";
+            break;
+        case 2:
+            cout << "\n Palembang";
+            break;
+        case 3:
+            cout << "\n Padang";
+            break;
+        case 4:
+            cout << "\n Bengkulu";
+            break;
+        default:
+            cout << "Kode Kota di Sumatra Salah";
+        }
+        break;
+    case 'K':
+        cout << "Masukan Kode Pulau Kalimantan ";
+        cin >> KodeKota;
+        switch (KodeKota)
+        {
+        case 1:
+            cout << "\n Banjarmasin";
+            break;
+        case 2:
+            cout << "\n Pontianak";
+            break;
+        case 3:
+            cout << "\n Lolong";
+            break;
+        default:
+            cout << "Kode Kota di Kalimantan Salah";
+        }
+        break;
+    default:
+        cout << "Kode Kota di Pulau Salah";
     }
-    else
-    {
-        temp_besar = y;
-    }
-
-    if (temp_besar > z)
-    {
-        temp_besar = temp_besar;
-    }
-    else
-    {
-        temp_besar = z;
-    }
-
-    if (temp_besar > a)
-    {
-        temp_besar = temp_besar;
-    }
-    else
-    {
-        temp_besar = a;
-    }
-
-    cout << temp_besar;
-
+    cout << endl;
     return 0;
 }
