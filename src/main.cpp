5a780a6cb4133edd2fcda652ff23c191dcc7035d
bimapriaaditya <bpabima@gmail.com>
2023-01-19 21:39:18 +0700

    clean up master branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,8 @@
 // main.cpp
 #include <iostream>
-// #include "uas/soal1/a.cpp"
-// #include "uas/soal1/b.cpp"
-// #include "uas/soal1/c.cpp"
-// #include "uas/soal1/d.cpp"
-
-// #include "uas/soal2/a.cpp"
-
-// #include "uas/soal3/a.cpp"
-// #include "uas/soal3/b.cpp"
-// #include "uas/soal3/c.cpp"
-// #include "uas/soal3/d.cpp"
-
-#include "uas/soal4/SortingArray.cpp"
 
 int main() 
 {
-  // ProgramA();
-  // ProgramB();
-  // ProgramC();
-  // ProgramD();
-
-  // Tugas2();
-
-  // ForLoop();
-  // WhileDo();
-  // DoWhile();
-  // NastedLoop();
-
-  SortingArray();
-
+  std::cout << "Lorem Ipsum";
   return 0;
 }
